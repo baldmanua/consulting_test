9f585fb536b7273402b29df9c514f2b134558c01
baldmanua <sasha.lysyi.1995@gmail.com>
2020-04-04 03:32:53 +0300

    nginx logs fix DB migrations

--- docker/database/migrations/0002_create_batches_table.sql
+++ docker/database/migrations/0002_create_batches_table.sql
@@ -1,9 +1,13 @@
 CREATE TABLE IF NOT EXISTS batches
 (
-    id               INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-    reference_number FLOAT NOT NULL,
-    date             DATE  NOT NULL,
-    merchant_id      INT UNSIGNED,
+    id          INT UNSIGNED AUTO_INCREMENT,
+    ref_num     FLOAT(24, 0) NOT NULL,
+    date        DATE         NOT NULL,
+    merchant_id BIGINT UNSIGNED,
+    PRIMARY KEY (id),
+    UNIQUE KEY ref_num_date_key (ref_num, date),
     FOREIGN KEY (merchant_id) REFERENCES merchants (id)
-) ENGINE = MYISAM;
-CREATE INDEX ref_num_date ON batches(reference_number, date);
+) ENGINE = MYISAM
+  DEFAULT CHARSET = utf8;
+
+CREATE INDEX ref_num_date_index ON batches (ref_num, date);
--- docker/database/migrations/0003_create_transactions_table.sql
+++ docker/database/migrations/0003_create_transactions_table.sql
@@ -1,11 +1,13 @@
 CREATE TABLE IF NOT EXISTS transactions
 (
-    id          INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    id          INT UNSIGNED AUTO_INCREMENT,
     date        DATE        NOT NULL,
-    type        VARCHAR(10) NOT NULL,
+    type        VARCHAR(20) NOT NULL,
     card_type   VARCHAR(2)  NOT NULL,
-    card_number VARCHAR(16) NOT NULL,
+    card_number VARCHAR(20) NOT NULL,
     amount      float       NOT NULL,
     batch_id    INT UNSIGNED,
+    PRIMARY KEY ('id'),
     FOREIGN KEY (batch_id) REFERENCES batches (id)
-) ENGINE = MYISAM;
+) ENGINE = MYISAM
+  DEFAULT CHARSET = utf8;
